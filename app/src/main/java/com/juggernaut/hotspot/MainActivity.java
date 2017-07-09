8b9b10f1c048dca920aac5a963810ed6533bef25
Kailash <kailashuniyal04@gmail.com>
2017-07-10 00:53:10 +0530

    Now working on Android M+ versions also

--- app/src/main/java/com/juggernaut/hotspot/MainActivity.java
+++ app/src/main/java/com/juggernaut/hotspot/MainActivity.java
@@ -1,15 +1,39 @@
 package com.juggernaut.hotspot;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
 
 public class MainActivity extends Activity implements View.OnClickListener {
+
+    public static String hotspotName = null;
     Button enableHotspot;
     Button disableHotspot;
     TextView textView;
@@ -17,10 +41,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
     Button state;
     EditText editText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         enableHotspot = (Button) findViewById(R.id.enableWifiHotSpotId);
         enableHotspot.setOnClickListener(this);
 
@@ -33,25 +59,36 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         disableHotspot = (Button) findViewById(R.id.disableWifiHotspotId);
         disableHotspot.setOnClickListener(this);
+
+    }
+    public void writePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(getApplicationContext())) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 200);
+
+            }
+        }
     }
 
     @Override
     public void onClick(View v) {
+        writePermission();
         switch (v.getId()) {
             case R.id.enableWifiHotSpotId:
                 try {
                     if (!((editText.getText().toString()).equals(""))) {
-                        OnOfHotspot.hotspotName = editText.getText().toString();
+                        hotspotName = editText.getText().toString();
                         OnOfHotspot.getApConfiguration(this);
                         OnOfHotspot.configApState(this, true);
-                    }else {
-                        Toast.makeText(getApplicationContext(),"Please Specify Hotspot name!",Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Please Specify Hotspot name!", Toast.LENGTH_LONG).show();
                     }
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 break;
+
             case R.id.disableWifiHotspotId:
                 try {
                     OnOfHotspot.getApConfiguration(this);
--- app/src/main/java/com/juggernaut/hotspot/OnOfHotspot.java
+++ app/src/main/java/com/juggernaut/hotspot/OnOfHotspot.java
@@ -3,18 +3,23 @@ package com.juggernaut.hotspot;
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import static com.juggernaut.hotspot.MainActivity.hotspotName;
+
 /**
  * Created by Kailash on 7/8/2017.
  */
+
 public class OnOfHotspot {
 
     public static WifiConfiguration config;
-    public static String hotspotName;
+
+
     //check whether wifi hotspot on or off
 
     public static boolean isApOn(Context context) {
@@ -37,17 +42,18 @@ public class OnOfHotspot {
             if (apState) {
                 WifiConfiguration netConfig = new WifiConfiguration();
 
-                netConfig.SSID = hotspotName;
+                netConfig.SSID = MainActivity.hotspotName;
                 netConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
 
-                Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+                Method method = WifiManager.class.getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
                 method.invoke(wifimanager, netConfig, apState);
-                Toast.makeText(context, "WiFi Hotspot \'" + config.SSID + "\' is Created!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "WiFi Hotspot is Created!", Toast.LENGTH_SHORT).show();
                 return true;
             } else {
-                Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+
+                Method method = WifiManager.class.getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
                 method.invoke(wifimanager, config, false);
-                Toast.makeText(context, "WiFi Hotspot \'" + config.SSID + "\' is Disabled!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "WiFi Hotspot is Disabled!", Toast.LENGTH_SHORT).show();
                 return true;
             }
         } catch (Exception e) {
@@ -56,22 +62,22 @@ public class OnOfHotspot {
         return false;
     }
 
-
     public static WifiConfiguration getApConfiguration(Context context) {
 
         WifiConfiguration config = null;
         WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        Method[] methods = wifimanager.getClass().getDeclaredMethods();
+        Method[] methods = WifiManager.class.getDeclaredMethods();
         for (Method m : methods) {
             if (m.getName().equals("getWifiApConfiguration")) {
                 try {
                     config = (WifiConfiguration) m.invoke(wifimanager);
                 } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-                    e.printStackTrace();
+                    Log.d("Testing", e.getCause().toString());
                 }
             }
         }
         OnOfHotspot.config = config;
         return config;
     }
+
 }
